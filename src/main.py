f1f4da2b3f9f87a7c7ee8fe65c8e94634c2e09ec
Gerald White <geraldkwhite10@gmail.com>
2022-11-14 11:10:17 -0600

    initial commit

--- src/main.py
+++ src/main.py
@@ -33,9 +33,9 @@ def run_model_one_example(example, model, tokenizer, first_and_second):
     first_second_sent_opt = [first_sents[i] + second_sent_opt[i] for i in range(10)]
 
     if first_and_second:
-        tk_example_opt = tokenizer(first_second_sent_opt, return_tensors="pt", padding=True).to(device)
+        tk_example_opt = tokenizer(first_second_sent_opt, return_tensors="pt", padding=True, truncate=True).to(device)
     else:
-        tk_example_opt = tokenizer(second_sent_opt, return_tensors="pt", padding=True).to(device)
+        tk_example_opt = tokenizer(second_sent_opt, return_tensors="pt", padding=True, truncate=True).to(device)
     output = model(**tk_example_opt, labels=tk_example_opt['input_ids'][label_idx].repeat(10, 1).to(device))
 
     tokenized = tk_example_opt ###
